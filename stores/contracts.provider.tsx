f41a6bbbc3236b88b1847e0434f10e40ad6262ef
José Vinícius <jvini_19@hotmail.com>
2025-02-05 23:41:05 -0300

    feat(stores): :sparkles: add types and status to useContracts

--- stores/contracts.provider.tsx
+++ stores/contracts.provider.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { ContractsContextType } from '@/interfaces/contexts.interface';
-import { ContractWithId } from '@/interfaces/contracts.interface';
+import { ContractWithId, Status, Type } from '@/interfaces/contracts.interface';
 import { createContext, useMemo, useState } from 'react';
 import React from 'react';
 
@@ -9,10 +9,12 @@ export const ContractsContext = createContext<ContractsContextType | undefined>(
 
 const ContractsProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const [contracts, setContracts] = useState<ContractWithId[]>([]);
+  const [status, setStatus] = useState<Status[]>([]);
+  const [type, setType] = useState<Type[]>([]);
 
   const contextValue: ContractsContextType = useMemo(
-    () => ({ setContracts, contracts }),
-    [contracts]
+    () => ({ setContracts, contracts, setType, type, setStatus, status }),
+    [contracts, type, status]
   );
 
   return <ContractsContext.Provider value={contextValue}>{children}</ContractsContext.Provider>;
