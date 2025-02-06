8768c4ba384e4f0f65ebf1cfec8aa8ce7f0df394
José Vinícius <jvini_19@hotmail.com>
2025-02-06 20:44:59 -0300

    fix(dashboard): :bug: improve dashboard metrics filters

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -10,14 +10,11 @@ export const Header = ({ children }: ChildrenProps) => {
 
   return (
     <div className="flex items-center justify-between">
-      <button
-        onClick={toggleSidebar}
-        className="-ml-2 mr-2 rounded-md p-2 hover:bg-accent md:hidden"
-      >
-        <Menu className="h-6 w-6" />
+      <button onClick={toggleSidebar} className="-ml-2 mr-2 rounded-md p-2 hover:bg-accent">
+        <Menu className="h-6 w-6 md:hidden" />
       </button>
 
-      <h1 className="text-2xl font-bold">{`${!isMobile ? 'Contract Management ' : 'C.M.'}Dashboard`}</h1>
+      <h1 className="text-2xl font-bold">{`${!isMobile ? 'Contract Management ' : ''}Dashboard`}</h1>
 
       {children}
     </div>
